907a493864e20d1b133323e1f45282e762d6372d
Heitor Raymundo <hbanhete@gmail.com>
2016-07-17 20:11:38 -0300

    utils de envio de email

---
+++ utils/email.py
@@ -0,0 +1,25 @@
+import cyclone.mail
+import cyclone.web
+
+me = None
+
+def init(api):
+    me = api.config['mail']['username']
+
+def any(req, api):
+    req.to = req.params['email']
+
+def send(req, subject, message=''):
+    msg = cyclone.mail.Message(
+            from_addr=me,
+            to_addrs=req.to,
+            subject=subject,
+            message=message,
+            mime=content_type,  # optional. default=text/plain
+            charset="utf-8")    # optional. default=utf-8
+
+    try:
+        response = yield cyclone.mail.sendmail(req.settings.email_settings, msg)
+        return True
+    except Exception, e:
+        return False
