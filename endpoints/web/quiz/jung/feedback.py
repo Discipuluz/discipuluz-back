181d5983152e04bcfe817ab6502c7fdc0fab5df5
diego <diego.kamiha17@gmail.com>
2016-09-04 18:07:24 -0300

    [05] Save user's feedback from Jung form

---
+++ endpoints/web/quiz/jung/feedback.py
@@ -0,0 +1,43 @@
+utils = [
+    'mongodb'
+]
+
+def post(req, api):
+    """
+    Save user's feedback from Jung forms
+
+    Input:
+        session: string
+        score: string
+        feedback: string
+
+    Output:
+        response:
+            error: boolean
+            session: string
+    """
+
+    session = api.mongodb.toObjectId(req.params["session"])
+    score = req.params["score"]
+    feedback = req.params["feedback"]
+
+    error = None
+
+    try:
+        index = api.mongodb.update(api, 'jung_answers', {'_id': session},{
+            'score': score,
+            'feedback': feedback
+        })
+    except Exception as e:
+        error = e
+
+    if not error:
+        req.send({
+            'error': False,
+            'session': str(index)
+        })
+    else:
+        req.send({
+            'error': True,
+            'message': str(error)
+        })
