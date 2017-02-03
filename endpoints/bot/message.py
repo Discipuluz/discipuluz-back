c2dbb0d917ac089fbff373c1667a1842e53bff2d
Karim Michel <karimmbk@MacBook-Pro-Karim.local>
2017-02-03 19:18:42 -0200

    Começo das checagens de message.py

--- endpoints/bot/message.py
+++ endpoints/bot/message.py
@@ -6,7 +6,7 @@ utils = [
 
 async def post(req, api):
     """
-    
+
     Input:
         id: string
         from: string
@@ -33,6 +33,35 @@ async def post(req, api):
     elif api.regex.match(r'(Conte-me sobre a )?Usp', text):
         university = api.mongodb.select(api, 'universities', {'name': 'Usp'}, ['description', 'public'])
         api.bot.message(api, user_id, email, university[0]['description'])
+
+    # MELHOR UNIVERSIDADE
+    match = api.regex.match(r'(?i)^melhores faculdades (.+)$', text)
+    elif match:
+        courses = api.mongodb.select(api, 'courses', {'name': {'$elemMatch':{'$eq': match.group(1)}}}, ['universities'])
+        universities = courses[0]['universities']
+        sorted(universities, key=lambda u: u['score'])
+        str_universities = ''
+        for university in universities:
+            str_universities += university['id'] + ': ' + str(university['score']) + '\n'
+        api.debug(str_universities)
+        # await api.bot.message(api, user_id, email, str_universities)
+
+    match = api.regex.match(r'(?i)^melhor faculdade (.+)$', text)
+    elif match:
+        universities = api.mongodb.select(api, 'courses', {
+            'name': {'$elemMatch':{'$eq': match.group(1)}}
+        })[0]['universities']
+        sorted(universities, key=lambda u: u['score'])
+        str_universities = ''
+        str_universities += universities[0]['x  id']+ '\n'
+        api.debug(str_universities)
+        # await api.bot.message(api, user_id, email, str_universities)
+
+    match = api.regex.match(r'(?i)^[aá]rea de atua[cç][ãa]o (.+)$', text)
+    elif match:
+        ocupation_area = api.mongodb.select(api, 'courses', {
+            'name':{'$elemMatch':{'$eq':match.group(1)}} #talvez nao seja 1
+        }, ['ocupation_area'])[0]['']
 
     return {
         'error': False
