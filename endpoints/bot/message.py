8d1dc13f282f2eef37da3d7eba05e41da2d430c5
Rodrigo Seiji Piubeli Hirao <rodrigo.seiji.hirao@gmail.com>
2017-02-03 21:23:29 -0200

    Added notify.

--- endpoints/bot/message.py
+++ endpoints/bot/message.py
@@ -22,46 +22,52 @@ async def post(req, api):
     email = req.params['from']
     text = req.params['content']
 
+    api.bot.notify(api, user_id, email, 'accepted')
+
     api.debug(user_id + ') User ' + email + ' send text: ' + text)
 
-    # MELHORES UNIVERSIDADES
-    match = api.regex.match(r'(?i)^melhores faculdades (.+)$', text)
-    if match:
-        universities = api.mongodb.select(api, 'courses', {
-            'name': {'$elemMatch':{'$eq': match.group(1)}}
-        }, ['universities'])[0]['universities']
-        sorted(universities, key=lambda u: u['score'])
-        str_universities = ''
-        for university in universities:
-            str_universities += university['id'] + ': ' + str(university['score']) + '\n'
-        api.debug(str_universities)
-        await api.bot.message(api, user_id, email, str_universities)
-        return {
-            'error': False
-        }
+    try :
+        # MELHORES UNIVERSIDADES
+        match = api.regex.match(r'(?i)melhores faculdades (.+)', text)
+        if match:
+            universities = api.mongodb.select(api, 'courses', {
+                'name': {'$elemMatch':{'$eq': match.group(1)}}
+            }, ['universities'])[0]['universities']
+            sorted(universities, key=lambda u: u['score'])
+            str_universities = ''
+            for university in universities:
+                str_universities += university['id'] + '\n'
+            await api.bot.message(api, user_id, email, str_universities)
+            return {
+                'error': False
+            }
 
-    # MELHOR UNIVERSIDADE
-    match = api.regex.match(r'(?i)^melhor faculdade (.+)$', text)
-    if match:
-        universities = api.mongodb.select(api, 'courses', {
-            'name': {'$elemMatch':{'$eq': match.group(1)}}
-        }, ['universities'])[0]['universities']
-        sorted(universities, key=lambda u: u['score'])
-        str_universities = ''
-        str_universities += universities[0]['id']+ '\n'
-        api.debug(str_universities)
-        await api.bot.message(api, user_id, email, str_universities)
-        return {
-            'error': False
-        }
-        
+        # MELHOR UNIVERSIDADE
+        match = api.regex.match(r'(?i)melhor faculdade (.+)', text)
+        if match:
+            universities = api.mongodb.select(api, 'courses', {
+                'name': {'$elemMatch':{'$eq': match.group(1)}}
+            }, ['universities'])[0]['universities']
+            sorted(universities, key=lambda u: u['score'])
+            str_universities = ''
+            str_universities += universities[0]['id']+ '\n'
+            await api.bot.message(api, user_id, email, str_universities)
+            return {
+                'error': False
+            }
+            
 
-    # AREA DE ATUACAO
-    match = api.regex.match(r'(?i)^[aá]rea de atua[cç][ãa]o (.+)$', text)
-    if match:
-        ocupation_area = api.mongodb.select(api, 'courses', {
-            'name':{'$elemMatch':{'$eq':match.group(1)}} #talvez nao seja 1
-        }, ['ocupation_area'])[0]['']
-        return {
-            'error': False
-        }
+        # AREA DE ATUACAO
+        match = api.regex.match(r'(?i)[aá]rea de atua[cç][ãa]o (.+)', text)
+        if match:
+            ocupation_area = api.mongodb.select(api, 'courses', {
+                'name':{'$elemMatch':{'$eq':match.group(1)}} #talvez nao seja 1
+            }, ['ocupation_area'])[0]['']
+            return {
+                'error': False
+            }
+    except Exception as e:
+        api.bot.notify(api, user_id, email, 'failed', {
+            'code': 00,
+            'description': str(error)
+        })
--- utils/bot.py
+++ utils/bot.py
@@ -6,12 +6,29 @@ import json
 
 async def message(api, user_id, email, text):
     with aiohttp.ClientSession() as session:
+        notify(api, user_id, email, 'dispatched')
         async with session.post(api.config['bot']['url']['messages'],
                         data=json.dumps({
                             'id': user_id,
                             'to': email,
                             'type': 'text/plain',
                             'content': text
+                        }),
+                        headers={
+                            'Content-Type': 'application/json',
+                            'Authorization': 'Key ' + api.config['bot']['token']
+                        }) as resp:
+            api.debug(resp.status)
+            api.debug(await resp.text())
+
+async def notify(api, user_id, email, event, error={}):
+    with aiohttp.ClientSession() as session:
+        async with session.post(api.config['bot']['url']['messages'],
+                        data=json.dumps({
+                            'id': user_id,
+                            'from': email,
+                            'content': event,
+                            'reason': error
                         }),
                         headers={
                             'Content-Type': 'application/json',
