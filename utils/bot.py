d2f86d6e584947382b1dfb6ebb896221fcb0caf6
Rodrigo Seiji Piubeli Hirao <rodrigo.seiji.hirao@gmail.com>
2017-02-03 15:09:09 -0200

    Fixed bot.

--- utils/bot.py
+++ utils/bot.py
@@ -5,16 +5,17 @@ import aiohttp
 import json
 
 async def message(api, user_id, email, text):
-    import json
-
-    await session.post(api.config['bot']['url']['messages'],
-                    data=json.dumps({
-                        'id': user_id,
-                        'to': email,
-                        'type': 'text/plain',
-                        'content': text
-                    }),
-                    headers={
-                        'Content-Type': 'application/json',
-                        'Authorization': 'Key ' + api.config['bot']['token']
-                    })
+    with aiohttp.ClientSession() as session:
+        async with session.post(api.config['bot']['url']['messages'],
+                        data=json.dumps({
+                            'id': user_id,
+                            'to': email,
+                            'type': 'text/plain',
+                            'content': text
+                        }),
+                        headers={
+                            'Content-Type': 'application/json',
+                            'Authorization': 'Key ' + api.config['bot']['token']
+                        }) as resp:
+            api.debug(resp.status)
+            api.debug(await resp.text())
